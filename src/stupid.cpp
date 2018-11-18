2e2f0ebd72704f66291d8c446e596928559165e7
pierfied <pierfied@email.arizona.edu>
2018-11-18 15:22:47 -0700

    Added empty source files.

---
+++ src/stupid.cpp
@@ -0,0 +1,7 @@
+#include "stupid.h"
+
+#include <iostream>
+
+void hello() {
+    std::cout << "Hello, World!" << std::endl;
+}
---
+++ src/stupid.h
@@ -0,0 +1,6 @@
+#ifndef STUPID2_LIBRARY_H
+#define STUPID2_LIBRARY_H
+
+void hello();
+
+#endif
