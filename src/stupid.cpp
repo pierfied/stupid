d821c0fb6ac3890f590c6ed402b0b5ec879a4ed6
pierfied <pierfied@email.arizona.edu>
2018-11-21 18:55:58 -0800

    Reorganized directory structure.

--- src/stupid.cpp
+++ src/stupid.cpp
@@ -1,37 +1,5 @@
-#include "stupid.h"
-#include "multidim_array.h"
-#include "particle_list.h"
-#include "cic_grid.h"
-
 #include <iostream>
 
 void hello() {
     std::cout << "Hello, World!" << std::endl;
-}
-
-int main() {
-    hello();
-
-    int num_particles = 10;
-    int num_dims = 3;
-
-    array_2d<double> x(num_particles, num_dims);
-    x(0, 0) = 1;
-    x(1, 1) = 2;
-
-    array_2d<double> p(num_particles, num_dims);
-
-    particle_list plist(x, p);
-
-    std::cout << plist.x(0, 0) << "\t\t" << plist.x(1, 1) << std::endl;
-
-    cic_grid a(2, 2, 2, plist);
-
-    x(0, 0) = -1;
-
-    std::cout << a.plist.x(0,0) << std::endl;
-
-    a.plist.x.reset_zero();
-
-    std::cout << a.plist.x(0,0) << std::endl;
 }
---
+++ test/test.cpp
@@ -0,0 +1,9 @@
+//
+// Created by pierfied on 11/21/18.
+//
+
+#include <stupid.h>
+
+int main() {
+    hello();
+}
