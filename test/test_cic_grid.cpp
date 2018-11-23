a88bdbefad64bc3a4f7134e32b2ac67a7833019d
pierfied <pierfied@email.arizona.edu>
2018-11-23 15:27:23 -0800

    Added more tests for cic_grid.

--- test/test_cic_grid.cpp
+++ test/test_cic_grid.cpp
@@ -42,6 +42,42 @@ TEST(cic_grid, flat_density) {
     }
 }
 
+TEST(cic_grid, flat_density_half_offset) {
+    int n = 10;
+
+    int num_particles = n * n * n;
+    int num_dims = 3;
+
+    array_2d<double> x(num_particles, num_dims);
+    array_2d<double> p(num_particles, num_dims);
+
+    int cur_particle = 0;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            for (int k = 0; k < n; ++k) {
+                x(cur_particle, 0) = i + 0.5;
+                x(cur_particle, 1) = j + 0.5;
+                x(cur_particle, 2) = k + 0.5;
+
+                cur_particle++;
+            }
+        }
+    }
+
+    particle_list plist(x, p);
+    cic_grid grid(n, n, n, plist, cosmology(0, 0, 0, 0));
+
+    grid.populate_delta_grid();
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            for (int k = 0; k < n; ++k) {
+                EXPECT_DOUBLE_EQ(0, grid.real_grid(i, j, k));
+            }
+        }
+    }
+}
+
 TEST(cic_grid, alternating_density) {
     int n = 10;
 
@@ -148,7 +184,7 @@ TEST(cic_grid, flat_potential) {
         }
     }
 
-    cosmology cosmo(0.3, 0, 0.7, 70);
+    cosmology cosmo(1, 0, 0, 0);
 
     particle_list plist(x, p);
     cic_grid grid(n, n, n, plist, cosmo);
@@ -159,7 +195,70 @@ TEST(cic_grid, flat_potential) {
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             for (int k = 0; k < n; ++k) {
-                EXPECT_EQ(0, grid.real_grid(i, j, k));
+                EXPECT_DOUBLE_EQ(0, grid.real_grid(i, j, k));
+            }
+        }
+    }
+}
+
+TEST(cic_grid, alternating_potential) {
+    int n = 10;
+
+    int num_particles = n * n * n;
+    int num_dims = 3;
+
+    array_2d<double> x(num_particles, num_dims);
+    array_2d<double> p(num_particles, num_dims);
+
+    int cur_particle = 0;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            for (int k = 0; k < n; ++k) {
+                x(cur_particle, 0) = i - i % 2;
+                x(cur_particle, 1) = j - j % 2;
+                x(cur_particle, 2) = k - k % 2;
+
+                cur_particle++;
+            }
+        }
+    }
+
+    cosmology cosmo(1, 0, 0, 0);
+
+    particle_list plist(x, p);
+    cic_grid grid(n, n, n, plist, cosmo);
+
+    double a = 1;
+    grid.compute_potential(a);
+
+    double center_potential = grid.real_grid(0, 0, 0);
+    double face_potential = grid.real_grid(0, 0, 1);
+    double edge_potential = grid.real_grid(0, 1, 1);
+    double corner_potential = grid.real_grid(1, 1, 1);
+
+    EXPECT_LT(center_potential, face_potential);
+    EXPECT_LT(face_potential, edge_potential);
+    EXPECT_LT(edge_potential, corner_potential);
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            for (int k = 0; k < n; ++k) {
+                int num_off = i % 2 + j % 2 + k % 2;
+
+                switch (num_off) {
+                    case 0:
+                        EXPECT_DOUBLE_EQ(center_potential, grid.real_grid(i, j, k));
+                        break;
+                    case 1:
+                        EXPECT_DOUBLE_EQ(face_potential, grid.real_grid(i, j, k));
+                        break;
+                    case 2:
+                        EXPECT_DOUBLE_EQ(edge_potential, grid.real_grid(i, j, k));
+                        break;
+                    case 3:
+                        EXPECT_DOUBLE_EQ(corner_potential, grid.real_grid(i, j, k));
+                        break;
+                }
             }
         }
     }
