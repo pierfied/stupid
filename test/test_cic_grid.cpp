06aec1c6bf208d1f40d421664949e3406cf90046
pierfied <pierfied@email.arizona.edu>
2018-11-22 23:44:39 -0800

    Added a second test case in which every eighth cell gets 8 particles. Passes.

--- test/test_cic_grid.cpp
+++ test/test_cic_grid.cpp
@@ -41,3 +41,43 @@ TEST(cic_grid, flat_density) {
         }
     }
 }
+
+TEST(cic_grid, alternating_density) {
+    int n = 10;
+
+    int num_particles = n * n * n;
+    int num_dims = 3;
+
+    array_2d<double> x(num_particles, num_dims);
+    array_2d<double> p(num_particles, num_dims);
+
+    int cur_particle = 0;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            for (int k = 0; k < n; ++k) {
+                x(cur_particle, 0) = i - i % 2;
+                x(cur_particle, 1) = j - j % 2;
+                x(cur_particle, 2) = k - k % 2;
+
+                cur_particle++;
+            }
+        }
+    }
+
+    particle_list plist(x, p);
+    cic_grid grid(n, n, n, plist);
+
+    grid.populate_delta_grid();
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            for (int k = 0; k < n; ++k) {
+                if (i % 2 == 0 && j % 2 == 0 && k % 2 == 0) {
+                    EXPECT_DOUBLE_EQ(7, grid.delta_grid(i, j, k));
+                } else {
+                    EXPECT_DOUBLE_EQ(-1, grid.delta_grid(i, j, k));
+                }
+            }
+        }
+    }
+}
