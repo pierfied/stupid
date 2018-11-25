625a4239ec21904363ff2d650ee25e663f744f2e
pierfied <pierfied@email.arizona.edu>
2018-11-24 20:01:39 -0800

    Added abstract integrator class.

---
+++ src/integrator.cpp
@@ -0,0 +1,5 @@
+//
+// Created by pierfied on 11/24/18.
+//
+
+#include "integrator.h"
---
+++ src/integrator.h
@@ -0,0 +1,20 @@
+//
+// Created by pierfied on 11/24/18.
+//
+
+#ifndef STUPID_INTEGRATOR_H
+#define STUPID_INTEGRATOR_H
+
+
+#include "grid.h"
+
+class integrator {
+    grid *g;
+
+    integrator(grid &g) : g(&g) {}
+
+    virtual void run_sim() = 0;
+};
+
+
+#endif //STUPID_INTEGRATOR_H
