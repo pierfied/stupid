31b7d160144a33989513ddbb59a4aefdc155628b
pierfied <pierfied@email.arizona.edu>
2018-11-25 01:56:14 -0800

    Fixed public vs private methods.

--- src/leapfrog_integrator.h
+++ src/leapfrog_integrator.h
@@ -9,18 +9,21 @@
 #include "integrator.h"
 #include <math.h>
 
-class leapfrog_integrator : integrator {
+class leapfrog_integrator : public integrator {
     const int num_steps;
     const double delta_a;
     const double a0;
 
+public:
     leapfrog_integrator(double a0, int num_steps, double delta_a, grid &g) : integrator(g), num_steps(num_steps),
                                                                              delta_a(delta_a), a0(a0) {}
 
     leapfrog_integrator(double a0, double a_final, double delta_a, grid &g) : integrator(g), delta_a(delta_a), a0(a0),
                                                                               num_steps(
-                                                                                      ceil((a_final - a0) / delta_a)) {}
+                                                                                      (int) ceil((a_final - a0) /
+                                                                                                 delta_a)) {}
 
+private:
     void forward_half_step_p(double a);
 
     void backward_half_step_p(double a);
