1b4dbced6e2f3d8b2016e8ccc4c68ae3a6aaade8
rafa <fisica2$>
2018-11-30 14:02:47 -0700

    Corrected plane wave collapse test. Still failing, but results look within 1% of analytical prediction.

--- test/test_problems.cpp
+++ test/test_problems.cpp
@@ -11,20 +11,21 @@
 #include <cosmology.h>
 
 
-TEST(leapfrog_integrator, one_dimensional_plane_wave) {
+TEST(problems, one_dimensional_plane_wave) {
     cosmology cosmo(1, 0, 0);
 
     int n = 32;
 
-    int num_particles = 64*64*64;
+    int npart = 16;
+    int num_particles = npart*npart*npart;
     int num_dims = 3;
 
     double a0 = 0.5;
-    double af = 5;
-    int num_steps = 100;
+    double af = 0.6;
+    int num_steps = 10000;
     double delta_a = (af - a0) / num_steps;
 
-    double across = 10*a0;
+    double across = 20*a0;
     double wave_num = 2*M_PI/n;
     double amplitude = 1./wave_num/cosmo.lgf(across);
 
@@ -32,16 +33,14 @@ TEST(leapfrog_integrator, one_dimensional_plane_wave) {
     array_2d<double> p(num_particles, num_dims);
 
     int cur_particle = 0;
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            for (int k = 0; k < n; ++k) {
-                x(cur_particle, 0) = double(i)*n/num_particles + cosmo.lgf(a0)*amplitude*sin(wave_num*i*n/num_particles);
-                x(cur_particle, 1) = double(j)*n/num_particles + cosmo.lgf(a0)*amplitude*sin(wave_num*j*n/num_particles);
-                x(cur_particle, 2) = double(k)*n/num_particles + cosmo.lgf(a0)*amplitude*sin(wave_num*k*n/num_particles);
+    for (int i = 0; i < npart; ++i) {
+        for (int j = 0; j < npart; ++j) {
+            for (int k = 0; k < npart; ++k) {
+                x(cur_particle, 0) = double(i)*n/npart + cosmo.lgf(a0)*amplitude*sin(wave_num*i*n/npart);
+                x(cur_particle, 1) = double(j)*n/npart;
+                x(cur_particle, 2) = double(k)*n/npart;
 
-                p(cur_particle, 0) = a0*cosmo.lgf(a0-0.5*delta_a*amplitude*sin(wave_num*i*n/num_particles));
-                p(cur_particle, 1) = a0*cosmo.lgf(a0-0.5*delta_a*amplitude*sin(wave_num*j*n/num_particles));
-                p(cur_particle, 2) = a0*cosmo.lgf(a0-0.5*delta_a*amplitude*sin(wave_num*k*n/num_particles));
+                p(cur_particle, 0) = a0*cosmo.lgf(a0-0.5*delta_a*amplitude*sin(wave_num*i*n/npart));
 
                 cur_particle++;
             }
@@ -56,12 +55,10 @@ TEST(leapfrog_integrator, one_dimensional_plane_wave) {
     li.run_sim();
 
     cur_particle = 0;
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            for (int k = 0; k < n; ++k) {
-                EXPECT_NEAR(i, x(cur_particle, 0), double(i)*n/num_particles + cosmo.lgf(af)*amplitude*sin(wave_num*i*n/num_particles));
-                EXPECT_NEAR(j, x(cur_particle, 1), double(j)*n/num_particles + cosmo.lgf(af)*amplitude*sin(wave_num*j*n/num_particles));
-                EXPECT_NEAR(k, x(cur_particle, 2), double(k)*n/num_particles + cosmo.lgf(af)*amplitude*sin(wave_num*k*n/num_particles));
+    for (int i = 0; i < npart; ++i) {
+        for (int j = 0; j < npart; ++j) {
+            for (int k = 0; k < npart; ++k) {
+                EXPECT_FLOAT_EQ(x(cur_particle, 0), double(i)*n/npart + cosmo.lgf(af)*amplitude*sin(wave_num*i*n/npart));
 
                 cur_particle++;
             }
