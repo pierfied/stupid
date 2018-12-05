04692e31983a78f4792c57ba7d857170bc835e4d
pierfied <pierfied@email.arizona.edu>
2018-12-04 21:57:09 -0700

    Update python wrapper to allow for outputting in real units.

--- stupid/stupid_wrapper.py
+++ stupid/stupid_wrapper.py
@@ -15,23 +15,36 @@ class _Args_Struct(Structure):
         ('Omega_m0', c_double),
         ('Omega_k0', c_double),
         ('Omega_l0', c_double),
+        ('sigma8', c_double),
         ('file_prefix', c_char_p),
         ('interp_scheme', c_int),
         ('integrator', c_int),
-        ('write_nth_step', c_int)
+        ('write_nth_step', c_int),
+        ('use_real_units', c_bool),
+        ('r0', c_double),
+        ('t0', c_double)
     ]
 
 
 class Cosmology:
-    def __init__(self, Omega_m0, Omega_k0, Omega_l0):
+    def __init__(self, Omega_m0, Omega_k0, Omega_l0, sigma8, H0):
         self.Omega_m0 = Omega_m0
         self.Omega_k0 = Omega_k0
         self.Omega_l0 = Omega_l0
+        self.sigma8 = sigma8
+        self.H0 = H0
 
 
 class STUPID:
-    def __init__(self, x, p, a0, af, delta_a, num_cells, cosmo, file_prefix, interp_scheme, integrator,
-                 write_nth_step):
+    def __init__(self, r, v, a0, af, delta_a, box_len, num_cells, cosmo, file_prefix, interp_scheme, integrator,
+                 write_nth_step, use_real_units=True):
+        self.r0 = box_len / num_cells
+        self.t0 = 1 / cosmo.H0
+        v0 = self.r0 / self.t0
+
+        x = r / (self.r0 * a0)
+        p = v * a0 / v0
+
         lib_path = os.path.join(os.path.dirname(__file__), 'libstupid.so')
         self.stupid_lib = cdll.LoadLibrary(lib_path)
 
@@ -48,6 +61,7 @@ class STUPID:
         self.cosmo = cosmo
         self.file_prefix = file_prefix
         self.write_nth_step = write_nth_step
+        self.use_real_units = use_real_units
 
         if interp_scheme == 'cic':
             self.interp_scheme = 1
@@ -73,10 +87,14 @@ class STUPID:
         args.Omega_m0 = self.cosmo.Omega_m0
         args.Omega_k0 = self.cosmo.Omega_k0
         args.Omega_l0 = self.cosmo.Omega_l0
+        args.sigma8 = self.cosmo.sigma8
         args.file_prefix = c_char_p(self.file_prefix.encode('utf-8'))
         args.interp_scheme = self.interp_scheme
         args.integrator = self.integrator
         args.write_nth_step = self.write_nth_step
+        args.use_real_units = self.use_real_units
+        args.r0 = self.r0
+        args.t0 = self.t0
 
         print('Starting Run')
 
